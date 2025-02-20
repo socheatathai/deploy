6aaf3d235025245578d83c48ca608d3b5aa02e80
socheatathai <socheatathai62@gmail.com>
2025-02-20 19:14:30 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersMemoryStorage } from './users-memory-storage/users-memory.storage';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env.local',
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService, UsersMemoryStorage],
